8669911484e86111058c4a293868778d495e7925
Ahmed <oublihi.a@gmail.com>
2023-02-15 13:01:24 +0100

    delete Employee

--- src/app/delete-employee/delete-employee.component.ts
+++ src/app/delete-employee/delete-employee.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Employee} from "../Employee";
+import { EmployeeService } from '../employee.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-delete-employee',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-employee.component.css']
 })
 export class DeleteEmployeeComponent {
+
+  @Output()
+  deleteEmployee: EventEmitter<Employee> = new EventEmitter<Employee>();
+
+  constructor(
+    private employeeService: EmployeeService, private router: Router
+  ) {
+  }
+
+  delete(employee: Employee) {
+    this.employeeService.deleteEmployee(employee.id!.toString());
+    this.deleteEmployee.emit(employee);
+    this.router.navigateByUrl('/employee');
+  }
 
 }
